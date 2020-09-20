14c193be6f5d58743893d1ab397cef3b3f51f0f0
Christina Howard <clhoward33@icloud.com>
2020-09-20 20:58:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
   end
 
   def apply_discount
-    if discount == 0
+    if @discount == 0
       return "There is no discount to apply."
     "After the discount, the total comes to $800."
   end
