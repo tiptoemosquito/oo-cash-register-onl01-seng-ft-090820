28ba8ceb7793f51ac1549c5bacc35b7bd67588af
Christina Howard <clhoward33@icloud.com>
2020-09-20 22:10:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,12 +11,13 @@ class CashRegister
 
 
   def add_item(title, price, quantity=1)
-    self.total += price * quantity
-    quantity.times do
-      items << title
-    end
-    self.last_transaction = amount * quantity
-  end
+     self.total += price * quantity
+     quantity.times do
+       items << title
+     end
+     self.last_transaction = price * quantity
+   end
+
 
 
   def apply_discount
