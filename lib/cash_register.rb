dc0c99e7658d60de2fd9fda84253bb5ab88378c8
Christina Howard <clhoward33@icloud.com>
2020-09-20 21:21:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,6 +14,7 @@ class CashRegister
     self.total += price * quantity
   end
 
+
   def apply_discount
     if discount != 0
       self.total = (total * ((100.0 - discount.to_f)/100)).to_i
@@ -23,7 +24,9 @@ class CashRegister
     end
   end
 
+
   def void_last_transaction
+    self.total = self.total - self.last_transaction
   end
 
 end
